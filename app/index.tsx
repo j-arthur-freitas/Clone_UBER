5516bd07175763b9cf16d57f92e80c16b78f799b
José Arthur Freitas da Rocha <j.arthur1711@gmail.com>
2025-11-19 21:11:31 -0300

    Remove unused NavigationContainer import
    Removed unused import statement for NavigationContainer.

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,5 @@
-// app/index.tsx (CORRETO)
 import 'react-native-reanimated';
 import React from 'react';
-// import { NavigationContainer } from '@react-navigation/native'; // ⬅️ REMOVIDO!
 import { AppNavigator } from '../src/navigation/AppNavigator';
 
 export default function AppRouterEntry() {
